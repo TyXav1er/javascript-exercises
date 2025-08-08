9295f5218cf1aaab2aeaf04a3aeb94c7f664e293
TyXav1er <Tyler.x.j2020@gmail.com>
2025-08-08 16:21:55 -0400

    Completed exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year % 4 === 0) {
+        return `The year ${year} is a leap year`
+    } else {
+        return `The year ${year} is not a leap year`
+    }
 };
+
+console.log(leapYears(2023))
 
 // Do not edit below this line
 module.exports = leapYears;
