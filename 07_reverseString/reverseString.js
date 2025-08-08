004a82887d3d0707043bf05cc7b93720aa3e8135
TyXav1er <Tyler.x.j2020@gmail.com>
2025-08-08 15:37:04 -0400

    Completed exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    return string = word.split("").reverse().join("")
 };
 
+console.log(reverseString("Dior"))
 // Do not edit below this line
 module.exports = reverseString;
