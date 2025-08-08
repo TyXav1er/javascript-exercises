278ebe1825b48ae61ddf2ba1735c113a530551ce
TyXav1er <Tyler.x.j2020@gmail.com>
2025-08-08 15:25:06 -0400

    I completed exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
+    let string = ""
 
+    for(let i = 0; i < count; i++){
+        string += word
+    }
+    return string
 };
+
+console.log(repeatString("Wazzahhhh", 3))
 
 // Do not edit below this line
 module.exports = repeatString;
