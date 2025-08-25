9e10fb24c4889ab491c589b72ad172bd25b26adc
TyXav1er <Tyler.x.j2020@gmail.com>
2025-08-25 12:34:11 -0400

    Completed Exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => {
+    return total + current
+  }, 0)
 };
 
-const multiply = function() {
+let arr = [1, 2, 3]
+console.log(sum(arr))
 
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+console.log(multiply(arr))
+
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let product = 1
+  for (let i = n; i > 0; i--) {
+    product *= 1
+  }
+  return product
 };
+
+console.log(factorial(4))
 
 // Do not edit below this line
 module.exports = {
