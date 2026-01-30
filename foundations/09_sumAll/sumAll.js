230878bce22e1b9d4b13bcb9036cb4adf709fad8
TyXav1er <Tyler.x.j2020@gmail.com>
2026-01-30 11:54:33 -0500

    Completed exercise 9 in javaScript exercisesa

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+        let total = 0;
 
+    for(let i = num1; i <= num2; i++) {
+        total += i
+
+    }
+    return total
 };
+
+console.log(sumAll(1, 10))
 
 // Do not edit below this line
 module.exports = sumAll;
