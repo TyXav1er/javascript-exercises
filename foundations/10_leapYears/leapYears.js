ba2d7a117db161da09ef8ab81fb8f7518e16f787
TyXav1er <Tyler.x.j2020@gmail.com>
2026-02-01 00:27:06 -0500

    Completed exercise 10 of javaScript exercises

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let isYearDivisibleByFour = year % 4 === 0
+  let isCentury = year % 100 === 0
+  let yearDivisibleByFourHundred = year % 400 === 0
 
+  if (isYearDivisibleByFour && (yearDivisibleByFourHundred || !isCentury)) {
+    return year + " is a leap year"
+  } else {
+    return year + " is not a leap year"
+  }
 };
+console.log(leapYears(400));
 
 // Do not edit below this line
 module.exports = leapYears;
