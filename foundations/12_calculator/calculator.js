49c14387eef26ef58852cc2622df46342ca5790c
TyXav1er <Tyler.x.j2020@gmail.com>
2026-02-12 10:15:27 -0500

    Completed exercises 12 & 13 in javascript exercises

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, currentItem) => 
+    total + currentItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
+
+console.log((2, 5));
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
